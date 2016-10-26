e8847375b27b1993d6fe2241f2629fb7a30a0f8e
aman <amanmehrotra112@gmail.com>
2016-10-27 01:11:42 +0530

    index file

--- alumni contents/js/index_js.js
+++ alumni contents/js/index_js.js
@@ -23,9 +23,41 @@
 
       	 document.getElementById("main-text").innerHTML=text[index];
         }
-
-    
-      
 });
+
+/*jquery starts here */
+
+$(document).ready(function(){
+  $(window).scroll(function() {
+  	/* Act on the event */
+  	// var outer=$('#home').height();
+  	 if($(document).scrollTop()>=200){
+  	 	$('#topnav').addClass('scrolled');
+  	 	$('#logo').removeClass('hide');
+  	 }
+  	 else{
+  	 	$('#topnav').removeClass('scrolled');
+  	 	$('#logo').addClass('hide');
+}
+  }).trigger('scroll');
+});
+
+/* Navigation bar scrolling */
+  $('#nav a[href=#]').click(function() {
+    return false;
+  });
+  
+  $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 
 })();
